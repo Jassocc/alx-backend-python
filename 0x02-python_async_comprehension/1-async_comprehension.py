a47755167a02808c0bb1525f4b654804eac46384
Jassocc <clnearle@gmail.com>
2024-04-23 08:29:40 +0000

    task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+script for task 1
+"""
+import typing
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    then return the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
