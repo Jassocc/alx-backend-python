bf378e63f0f5b113787e3eb4038a3e427b320b8d
Jassocc <clnearle@gmail.com>
2024-04-23 10:04:21 +0000

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,13 @@ sxript for task 0
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     asynchronously wait 1 second
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
