19ef791f9a86d758d2f09dc52ca9bd350ce7db27
Jassocc <clnearle@gmail.com>
2024-04-23 08:25:51 +0000

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+sxript for task 0
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    asynchronously wait 1 second
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
